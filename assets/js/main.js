8b132490b60bebb46641e6db525491c657e2eb33
iizexii <iizexii@gmail.com>
2021-10-27 12:45:13 +0300

    добавил независимые рейтинги

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,4 +3,42 @@
 $(document).ready(() => {
 	$("body #header").load("/header.html");
 	$("body #footer").load("/footer.html");
+	$(".owl-carousel").owlCarousel({
+		loop: true,
+		smartSpeed: 450,
+		autoplay: false,
+		nav: true,
+		dots: false,
+		navText: ["", ""],
+		responsive: {
+			0: {
+				items: 1,
+				center: true,
+				margin: 40,
+			},
+			361: {
+				items: 2,
+				center: true,
+				margin: 10,
+			},
+			481: {
+				items: 2,
+				center: true,
+				margin: 30,
+			},
+			641: {
+				items: 3,
+				center: true,
+				margin: 30,
+			},
+			769: {
+				items: 4,
+				margin: 20,
+			},
+			1000: {
+				items: 5,
+				margin: 10,
+			}
+		}
+	});
 })
